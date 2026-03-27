45421cf17e4ae7f857fb70b4912380c55511719c
Dens <dadadadenns1@gmail.com>
2026-03-27 15:17:29 +1000

    final Push

--- src/Main.java
+++ src/Main.java
@@ -3,59 +3,110 @@
 public class Main {
     public static void main(String[] args) {
 
-     //dog paper cat part
-     var dog = 8.0;
-     var cat = 3.6;
-     var paper = 763789;
-     System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
-     dog = dog + 4;
-     cat = cat + 4;
-     paper = paper + 4;
-     System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
-     dog = dog - 3.5;
-     cat = cat - 1.6;
-     paper = paper - 7639;
-     System.out.println("dog = " + dog + " cat = " + cat + " paper = " + paper);
+        // part 1
+        float flota = 33.1234567f;
+        double doblota = 10.123456789101214156;
+        long longota = 1234567891012141619L;
+        short shrota = 15000;
+        byte bytota = 100;
+        int intota = 1000000000;
+        System.out.println("flota = " + flota + " longota = " + longota + " shrota = " + shrota + " bytota = " + bytota + " intota = " + intota + " doblota = " + doblota);
 
-     //friend part
-     var friend = 19;
-     System.out.println("friend = " + friend);
-     friend = friend + 2;
-     System.out.println("friend = " + friend);
-     friend = friend / 7;
-     System.out.println("friend = " + friend);
+        //part 2
+        flota = 27.12f;
+        longota = 987678965549L;
+        float flota2 = 2.786f;
+        shrota = 569;
+        short shrota2 = -159;
+        short shrota3 = 27897;
+        bytota = 67;
+        System.out.println("flota = " + flota + " longota = " + longota + " flota2 = " + flota2 + " shrota = " + shrota + " shrota2 = " + shrota2 + " shrota3 = " + shrota3 + " bytota = " + bytota);
 
-     //frog part
-     var frog = 3.5;
-     System.out.println("frog = " + frog);
-     frog = frog * 10;
-     System.out.println("frog = " + frog);
-     frog = frog / 3.5;
-     System.out.println("frog = " + frog);
-     frog = frog + 4;
-     System.out.println("frog = " + frog);
+        //part 3
+        byte ludmillaStudents = 23;
+        byte annaStudents = 27;
+        byte ekaterinaStudents = 30;
+        short sheetsOfPaper = 480;
+        //Конец условных данных
+        System.out.println("Учеников у Людмилы " + ludmillaStudents + ", Анны " + annaStudents + ", Екатерины " + ekaterinaStudents + "; а также было закуплено " + sheetsOfPaper + " листов бумаги");
+        short sumStudents = (short) (ludmillaStudents + ekaterinaStudents + annaStudents);
+        byte sheetsPerPerson = (byte) (sheetsOfPaper / sumStudents);
+        System.out.println("Всего учеников " + sumStudents + " и на каждого будет по " + sheetsPerPerson + " листа бумаги");
 
-     //boxing part
-     var firstBoxerWeightKg = 78.2;
-     var secondBoxerWeightKg = 82.7;
-     System.out.println("Первый боксер весит = " + firstBoxerWeightKg + " кг Второй боксер = " + secondBoxerWeightKg + " кг");
-     var summOfTwoBoxers = firstBoxerWeightKg + secondBoxerWeightKg;
-     System.out.println("Масса двух боксеров = " + summOfTwoBoxers + " кг");
-     var weightDifference = secondBoxerWeightKg - firstBoxerWeightKg;
-     System.out.println("Разница в весе = " + weightDifference + " кг");
-     var modulusWeight = secondBoxerWeightKg % firstBoxerWeightKg;
-     System.out.println("Остаток от деления между двумя весами = " + modulusWeight);
+        //part 4
+        byte bottlesPerTwoMinutes = 16;//условие кол-во бутылок за 2 минуты
+        byte bottlesPerMinute = 16 / 2;
+        System.out.println("Производительность машины для бутылок - " + bottlesPerTwoMinutes + " бутылок за две минуты, или " + bottlesPerMinute + " за минуту");
+        int time = 20;//условие за 20 минут
+        int bottlesProduced = bottlesPerMinute * time;
+        System.out.println("За " + time + " минут машина произвела " + bottlesProduced + " штук бутылок");
+        time = 24 * 60;//условие за день
+        bottlesProduced = bottlesPerMinute * time;
+        System.out.println("За " + time + " минут или день машина произвела " + bottlesProduced + " штук бутылок");
+        time = time * 3;//условие за 3 дня
+        bottlesProduced = bottlesPerMinute * time;
+        System.out.println("За " + time + " минут или 3 дня машина произвела " + bottlesProduced + " штук бутылок");
+        time = time * 10;//условие за месяц
+        bottlesProduced = bottlesPerMinute * time;
+        System.out.println("За " + time + " минут или 30 дней, или месяц машина произвела " + bottlesProduced + " штук бутылок");
 
-     //work time part
-     var manHours = 640.0;
-     System.out.println("Человекочасы в кампании " + manHours + " часов");
-     var workTime = 8.0;
-     System.out.println("Каждый работает по " + workTime + " часов");
-     var workers = manHours / workTime;
-     System.out.println("Всего работников в компании — " + workers + " человек");
-     workers = workers + 94.0;
-     workTime = manHours / workers;
-     System.out.println("Если в компании работает " + workers + " человек, то всего " + workTime + " часов работы может быть поделено между сотрудниками");
-     //Если без точек, тогда ответ получается по 3 часа на 174 сотрудника, а это всего 522 человеко-часов, что не верно.
+        //part 5
+        byte cansOfPaintRequired = 120;
+        byte whitePaintPerClass = 2;
+        byte brownPaintPerClass = 4;
+        //Конец условных данных
+        System.out.println("Для покраски 1-го класса нужно " + whitePaintPerClass + " банок белой краски и " + brownPaintPerClass + " банок коричневой, а всего банок нужно " + cansOfPaintRequired);
+        float ratioBrownToWhitePaint = (float) (brownPaintPerClass / whitePaintPerClass);
+        byte whitePaintRequired = (byte) (cansOfPaintRequired / (1 + ratioBrownToWhitePaint));
+        byte brownPaintRequired = (byte) (whitePaintRequired * ratioBrownToWhitePaint);
+        byte numberOfClasses = (byte) (cansOfPaintRequired / (whitePaintPerClass + brownPaintPerClass));
+        System.out.println("В школе, где " + numberOfClasses + " классов, нужно " + whitePaintRequired + " банок белой краски и " + brownPaintRequired + " банок коричневой краски");
+
+        //part 6
+        byte bananasAmount = 5;
+        byte gramsPerBanana = 80;
+        short milkMl = 200;
+        float milkInGramsPer100Ml = 105f;
+        byte iceCreamBarAmount = 2;
+        byte gramsPerIceCreamBar = 100;
+        byte rawEggsAmount = 4;
+        byte gramsPerEgg = 70;
+        //Конец условных данных
+        System.out.println("Кол-во и масса ингредиентов: " + bananasAmount + " бананов по " + gramsPerBanana + "г/шт; " + milkMl + "мл молока по " + milkInGramsPer100Ml + "г/100 мл; " + iceCreamBarAmount + " брикетов мороженного по " + gramsPerIceCreamBar + " г/шт; " + rawEggsAmount + " сырых яиц по " + gramsPerEgg + " г/шт.");
+        float totalWeightInGrams = (gramsPerBanana * bananasAmount + milkMl * milkInGramsPer100Ml / 100 + gramsPerIceCreamBar * iceCreamBarAmount + rawEggsAmount * gramsPerEgg);
+        float totalWeightInKg = (totalWeightInGrams / 1000);
+        System.out.println("Вес коктейля в граммах " + totalWeightInGrams + ", вес в кг " + totalWeightInKg);
+
+        //part 7
+        byte weightToLoseKg = 7;
+        short minGramsPerDayToLose = 250;
+        short maxGramsPerDayToLose = 500;
+        //Конец условных данных
+        System.out.println("Нужно сбросить - " + weightToLoseKg + " кг, Тренел дал рацион который в лучше случае даст худеть на " + maxGramsPerDayToLose + " грамм за день, в худшем " + minGramsPerDayToLose + " грамм");
+        short weightToLoseGrams = (short) (weightToLoseKg * 1000);
+        byte maxDaysToLoseWeight = (byte) (weightToLoseGrams / minGramsPerDayToLose);
+        byte minDaysToLoseWeight = (byte) (weightToLoseGrams / maxGramsPerDayToLose);
+        float averageDaysToLoseWeight = ((float) (maxDaysToLoseWeight + minDaysToLoseWeight) / 2);
+        System.out.println("Максимально быстро можно похудеть спортсмену за " + minDaysToLoseWeight + " дней, максимально долго " + maxDaysToLoseWeight + " дней, в среднем " + averageDaysToLoseWeight + " дней");
+
+        //part 8
+        int masha = 67760;//Текущие зарплаты masha, denis, kristina в месяц
+        int denis = 83690;
+        int kristina = 76230;
+        float salaryIncrease = 1.1f;
+        //Конец условных данных
+        int mashaPerYearOld = masha * 12;
+        int denisPerYearOld = denis * 12;
+        int kristinaPerYearOld = kristina * 12;
+        System.out.println("Маша в месяц получает " + 67760 + " руб, Денис - " + denis + ", Кристина - " + kristina);
+        masha = (int) (masha * salaryIncrease);// Зарплаты в месяц после повышения
+        denis = (int) (denis * salaryIncrease);
+        kristina = (int) (kristina * salaryIncrease);
+        System.out.println("Их зарплата в месяц увеличится в " + salaryIncrease + ". Итого Маша будет получать в месяц " + masha + " руб, Денис - " + denis + ", Кристина - " + kristina);
+        masha = masha * 12 - mashaPerYearOld;//Разница годовых зарплат, новой минус старой
+        denis = denis * 12 - denisPerYearOld;
+        kristina = kristina * 12 - kristinaPerYearOld;
+        System.out.println("Прибавка к годовой зарплате у Маши - " + masha + ", у Дениса - " + denis + ", Кристины - " + kristina);
+
     }
 }
