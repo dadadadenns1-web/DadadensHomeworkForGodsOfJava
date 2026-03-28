0df9aa31fa45d124702b3f4670f4eb2fb373aa27
Dens <dadadadenns1@gmail.com>
2026-03-28 18:45:40 +1000

    homework 3 "first push"

--- src/Main.java
+++ src/Main.java
@@ -3,109 +3,85 @@
 public class Main {
     public static void main(String[] args) {
 
-        // part 1
-        float flota = 33.1234567f;
-        double doblota = 10.123456789101214156;
-        long longota = 1234567891012141619L;
-        short shrota = 15000;
-        byte bytota = 100;
-        int intota = 1000000000;
-        System.out.println("flota <<float>> = " + flota + ", longota <<long>> = " + longota + ", shrota <<short>> = " + shrota + ", bytota <<byte>> = " + bytota + ", intota <<int>> = " + intota + ", doblota <<double>> = " + doblota);
+        //part 1
+        short userAgePart1 = 23; //изменяемая переменная
+        byte ageOfMajority = 18;
+        if (userAgePart1 >= ageOfMajority) {
+            System.out.println("Если возраст человека равен " + userAgePart1 + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + userAgePart1 + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
 
         //part 2
-        flota = 27.12f;
-        longota = 987678965549L;
-        float flota2 = 2.786f;
-        shrota = 569;
-        short shrota2 = -159;
-        short shrota3 = 27897;
-        bytota = 67;
-        System.out.println("flota <<float>> = " + flota + ", longota <<long>> = " + longota + ", flota2 <<float>> = " + flota2 + ", shrota <<short>> = " + shrota + ", shrota2 <<short>> = " + shrota2 + ", shrota3 <<short>> = " + shrota3 + ", bytota <<byte>> = " + bytota);
+        byte airTemperatureDegrees = 2; //изменяемая переменная
+        byte minTemperatureWithoutHat = 5;
+        if (airTemperatureDegrees < minTemperatureWithoutHat) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+            System.out.println("На улице " + airTemperatureDegrees + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице тепло, можно идти без шапки");
+            System.out.println("На улице " + airTemperatureDegrees + " градусов, можно идти без шапки");
+        }
 
         //part 3
-        byte ludmillaStudents = 23;
-        byte annaStudents = 27;
-        byte ekaterinaStudents = 30;
-        short sheetsOfPaper = 480;
-        //Конец условных данных
-        System.out.println("Учеников у Людмилы " + ludmillaStudents + ", Анны " + annaStudents + ", Екатерины " + ekaterinaStudents + "; а также было закуплено " + sheetsOfPaper + " листов бумаги");
-        short sumStudents = (short) (ludmillaStudents + ekaterinaStudents + annaStudents);
-        byte sheetsPerPerson = (byte) (sheetsOfPaper / sumStudents);
-        System.out.println("Всего учеников " + sumStudents + " и на каждого будет по " + sheetsPerPerson + " листа бумаги");
+        short carSpeed = 100; //изменяемая переменная
+        short speedLimit = 60;
+        if (carSpeed > speedLimit) {
+            System.out.println("Если скорость " + carSpeed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + carSpeed + ", то можно ездить спокойно");
+        }
 
         //part 4
-        byte bottlesPerTwoMinutes = 16;//условие кол-во бутылок за 2 минуты
-        byte bottlesPerMinute = 16 / 2;
-        System.out.println("Производительность машины для бутылок - " + bottlesPerTwoMinutes + " бутылок за две минуты, или " + bottlesPerMinute + " за минуту");
-        int time = 20;//условие за 20 минут
-        int bottlesProduced = bottlesPerMinute * time;
-        System.out.println("За " + time + " минут машина произвела " + bottlesProduced + " штук бутылок");
-        time = 24 * 60;//условие за день
-        bottlesProduced = bottlesPerMinute * time;
-        System.out.println("За " + time + " минут или день машина произвела " + bottlesProduced + " штук бутылок");
-        time = time * 3;//условие за 3 дня
-        bottlesProduced = bottlesPerMinute * time;
-        System.out.println("За " + time + " минут или 3 дня машина произвела " + bottlesProduced + " штук бутылок");
-        time = time * 10;//условие за месяц
-        bottlesProduced = bottlesPerMinute * time;
-        System.out.println("За " + time + " минут или 30 дней, или месяц машина произвела " + bottlesProduced + " штук бутылок");
+        byte minAgeForWork = 25;
+        byte minAgeForUniversity = 18;
+        byte minAgeForSchool = 7;
+        byte minAgeForKindergarten = 2;
+        short userAgePart4 = 17; //изменяемая переменная
+        if (userAgePart4 >= minAgeForWork) {
+            System.out.println("Если возраст человека равен " + userAgePart4 + ", то ему нужно ходить на работу");
+        } else if (userAgePart4 >= minAgeForUniversity) {
+            System.out.println("Если возраст человека равен " + userAgePart4 + ", то ему нужно ходить в университет");
+        } else if (userAgePart4 >= minAgeForSchool) {
+            System.out.println("Если возраст человека равен " + userAgePart4 + ", то ему нужно ходить в школу");
+        } else if (userAgePart4 >= minAgeForKindergarten) {
+            System.out.println("Если возраст человека равен " + userAgePart4 + ", то ему нужно ходить в детский сад");
+        }
 
         //part 5
-        byte cansOfPaintRequired = 120;
-        byte whitePaintPerClass = 2;
-        byte brownPaintPerClass = 4;
-        //Конец условных данных
-        System.out.println("Для покраски 1-го класса нужно " + whitePaintPerClass + " банок белой краски и " + brownPaintPerClass + " банок коричневой, а всего банок нужно " + cansOfPaintRequired);
-        float ratioBrownToWhitePaint = ((float) brownPaintPerClass / whitePaintPerClass);
-        byte whitePaintRequired = (byte) (cansOfPaintRequired / (1 + ratioBrownToWhitePaint));
-        byte brownPaintRequired = (byte) (whitePaintRequired * ratioBrownToWhitePaint);
-        byte numberOfClasses = (byte) (cansOfPaintRequired / (whitePaintPerClass + brownPaintPerClass));
-        System.out.println("В школе, где " + numberOfClasses + " классов, нужно " + whitePaintRequired + " банок белой краски и " + brownPaintRequired + " банок коричневой краски");
+        byte noRestrictionAge = 15;
+        byte minAgeForAttraction = 6;
+        byte childAge = 6; //изменяемая переменная
+        if (childAge >= noRestrictionAge) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+        } else if (childAge >= minAgeForAttraction) {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        } else {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему нельзя кататься на аттракционе");
+        }
 
         //part 6
-        byte bananasAmount = 5;
-        byte gramsPerBanana = 80;
-        short milkMl = 200;
-        float milkInGramsPer100Ml = 105f;
-        byte iceCreamBarAmount = 2;
-        byte gramsPerIceCreamBar = 100;
-        byte rawEggsAmount = 4;
-        byte gramsPerEgg = 70;
-        //Конец условных данных
-        System.out.println("Кол-во и масса ингредиентов: " + bananasAmount + " бананов по " + gramsPerBanana + "г/шт; " + milkMl + "мл молока по " + milkInGramsPer100Ml + "г/100 мл; " + iceCreamBarAmount + " брикетов мороженного по " + gramsPerIceCreamBar + " г/шт; " + rawEggsAmount + " сырых яиц по " + gramsPerEgg + " г/шт.");
-        float totalWeightInGrams = (gramsPerBanana * bananasAmount + milkMl * milkInGramsPer100Ml / 100 + gramsPerIceCreamBar * iceCreamBarAmount + rawEggsAmount * gramsPerEgg);
-        float totalWeightInKg = (totalWeightInGrams / 1000);
-        System.out.println("Вес коктейля в граммах " + totalWeightInGrams + ", вес в кг " + totalWeightInKg);
+        short passengerCarriageCapacity = 102;
+        short passengerCarriageSeats = 60;
+        short passengersInCarriage = 60; //изменяемая переменная
+        if (passengersInCarriage >= passengerCarriageCapacity) {
+            System.out.println("В вагоне нет места для новых пассажиров");
+        } else if (passengersInCarriage >= passengerCarriageSeats) {
+            System.out.println("В вагоне есть только стоячие места");
+        } else {
+            System.out.println("В вагоне есть сидячие места");
+        }
 
         //part 7
-        byte weightToLoseKg = 7;
-        short minGramsPerDayToLose = 250;
-        short maxGramsPerDayToLose = 500;
-        //Конец условных данных
-        System.out.println("Нужно сбросить - " + weightToLoseKg + " кг, Тренел дал рацион который в лучше случае даст худеть на " + maxGramsPerDayToLose + " грамм за день, в худшем " + minGramsPerDayToLose + " грамм");
-        short weightToLoseGrams = (short) (weightToLoseKg * 1000);
-        byte maxDaysToLoseWeight = (byte) (weightToLoseGrams / minGramsPerDayToLose);
-        byte minDaysToLoseWeight = (byte) (weightToLoseGrams / maxGramsPerDayToLose);
-        float averageDaysToLoseWeight = ((float) (maxDaysToLoseWeight + minDaysToLoseWeight) / 2);
-        System.out.println("Максимально быстро можно похудеть спортсмену за " + minDaysToLoseWeight + " дней, максимально долго " + maxDaysToLoseWeight + " дней, в среднем " + averageDaysToLoseWeight + " дней");
-
-        //part 8
-        int masha = 67760;//Текущие зарплаты masha, denis, kristina в месяц
-        int denis = 83690;
-        int kristina = 76230;
-        float salaryIncrease = 1.1f;
-        //Конец условных данных
-        int mashaPerYearOld = masha * 12;
-        int denisPerYearOld = denis * 12;
-        int kristinaPerYearOld = kristina * 12;
-        System.out.println("Маша в месяц получает " + 67760 + " руб, Денис - " + denis + ", Кристина - " + kristina);
-        masha = (int) (masha * salaryIncrease);// Зарплаты в месяц после повышения
-        denis = (int) (denis * salaryIncrease);
-        kristina = (int) (kristina * salaryIncrease);
-        System.out.println("Их зарплата в месяц увеличится в " + salaryIncrease + ". Итого Маша будет получать в месяц " + masha + " руб, Денис - " + denis + ", Кристина - " + kristina);
-        masha = masha * 12 - mashaPerYearOld;//Разница годовых зарплат, новой минус старой.
-        denis = denis * 12 - denisPerYearOld;
-        kristina = kristina * 12 - kristinaPerYearOld;
-        System.out.println("Годовой доход вырос у Маши - " + masha + ", у Дениса - " + denis + ", Кристины - " + kristina);
+        int one = 213; //изменяемая переменная
+        int two = 25; //изменяемая переменная
+        int three = 23; //изменяемая переменная
+        if (one > two && one > three) {
+            System.out.println(one + " - самое большое число");
+        } else if (two > three) {
+            System.out.println(two + " - самое большое число");
+        } else {
+            System.out.println(three + " - самое большое число");
+        }
     }
 }
