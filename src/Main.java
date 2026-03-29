d8878b31f541d6f77201b7fd669f98dbdf3cbd1a
Dens <dadadadenns1@gmail.com>
2026-03-29 22:30:17 +1000

    "homework 4" Изменен порядок вычислений

--- src/Main.java
+++ src/Main.java
@@ -57,7 +57,7 @@ public class Main {
             System.out.println("Потребуется дней: " + daysToDeliver);
         }
 
-        //part 5
+        //part 5.
 
         byte monthNumber = 12; //изменяемая переменная 1 2 12 - зима; 3 4 5 - весна; 6 7 8 - лето; 9 10 11 - осень;
         switch (monthNumber) {
