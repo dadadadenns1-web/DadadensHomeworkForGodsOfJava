e9c3c60c1c2b55d80f137c0b22f9f6fa063365fc
Dens <dadadadenns1@gmail.com>
2026-04-01 06:55:51 +1000

    "homework 8" firstPush

--- src/Main.java
+++ src/Main.java
@@ -6,52 +6,73 @@ public class Main {
     public static void main(String[] args) {
 
         //part 1
-        byte[] massiveByte = new byte[]{1, 2, 3};
-
-        float[] massiveFloat = {1.57f, 7.654f, 9.986f};
-
-        short[] massiveShort = new short[]{111, 222, 333, 444, 555, 666, 777, 888, 999};
-
-        //part 2
-        for (int i = 0; i < massiveByte.length - 1; i++) {//i начинается с первого элемента, если длина больше 2-ух
-            System.out.print(massiveByte[i] + ", ");
-        }
-        System.out.println(massiveByte[massiveByte.length - 1]); //Последний элемент массива
-
-        for (int i = 0; i < massiveFloat.length - 1; i++) {
-            System.out.print(massiveFloat[i] + ", ");
-        }
-        System.out.println(massiveFloat[massiveFloat.length - 1]);
-
-        for (int i = 0; i < massiveShort.length - 1; i++) {
-            System.out.print(massiveShort[i] + ", ");
-        }
-        System.out.println(massiveShort[massiveShort.length - 1]);
-
-        //part 3
-        for (int i = massiveByte.length - 1; i > 0; i--) {//i начинается с последнего элемента, если длина больше 2-ух
-            System.out.print(massiveByte[i] + ", ");
-        }
-        System.out.println(massiveByte[0]);//первый элемент массива
-
-        for (int i = massiveFloat.length - 1; i > 0; i--) {
-            System.out.print(massiveFloat[i] + ", ");
-        }
-        System.out.println(massiveFloat[0]);
-
-        for (int i = massiveShort.length - 1; i > 0; i--) {
-            System.out.print(massiveShort[i] + ", ");
-        }
-        System.out.println(massiveShort[0]);
-
-        //part 4 Только первый массив
-        for (int i = 0; i < massiveByte.length; i++) {
-            if (massiveByte[i] % 2 != 0) {
-                massiveByte[i]++;
+        System.out.println("Задача 1");
+        int[] inputArray1 = {15555, 22222, 33333, 25111, 12121};
+        int maxPay = inputArray1[0];
+        int sum = 0;
+        int minPay = inputArray1[0];
+        for (int input : inputArray1) {
+            sum += input;
+            if (maxPay < input) {
+                maxPay = input;
+            }
+            if (minPay > input) {
+                minPay = input;
             }
         }
-        //Проверка
-        System.out.println(Arrays.toString(massiveByte));
+        float average = (float) sum / inputArray1.length;
+        float[] outputArray1 = {sum, maxPay, minPay, average};
+        System.out.println(Arrays.toString(inputArray1) + " - input array; выплаты авторам");
+        System.out.println(Arrays.toString(outputArray1) + " - output array. Сумма, макс, мин, средне-арифметическая.");
 
+        //part 2
+        System.out.println("Задача 2");
+        int[] inputArray2 = {23232, 35254, 50000, 70000, 99999};
+        float[] outputArray2 = new float[inputArray2.length];
+        int indexPart2 = 0;
+        for (int salary : inputArray2) {
+            outputArray2[indexPart2++] = salary * 0.13f;//Нужно записать налог 13%
+        }
+        System.out.println(Arrays.toString(inputArray2) + " - input array; значение зарплаты");
+        System.out.println(Arrays.toString(outputArray2) + " - output array; значение налога 13%");
+
+        //part 3
+        System.out.println("Задача 3");
+        int[] inputArray3 = {1500, 2322, 7501, 2312, 12312};//Бонусы
+        boolean[] outputArray3 = new boolean[inputArray3.length];
+        int bonusSeparator = 5000;
+        int indexPart3 = 0;
+        for (int bonus : inputArray3) {
+            outputArray3[indexPart3++] = bonus > bonusSeparator;
+        }
+        System.out.println(Arrays.toString(inputArray3) + " - input array; бонусы");
+        System.out.println(Arrays.toString(outputArray3) + " - output array; true если больше " + bonusSeparator);
+
+        //part 4
+        System.out.println("Задача 4");
+        int[] inputArray4 = {12312, 2562, -1231, 5233, 6233};//Остаток на кредитном счете
+        boolean[] outputArray4 = {true};
+        for (int balance : inputArray4) {
+            if (balance < 0) {
+                outputArray4[0] = false;
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(inputArray4) + " - input array; все остатки в начале месяцев");
+        System.out.println(Arrays.toString(outputArray4) + " - output array; false - есть отрицательный остаток, true - все положительные");
+
+        //part 5
+        System.out.println("Задача 5");
+        int[] inputArray5 = {1234, 12512, -3231, -10000, 1000};//чистая прибыль
+        int[] outputArray5 = {0};//Количество месяцев рентабельности
+
+        for (int profit : inputArray5) {
+            if (profit > 0) {
+                outputArray5[0]++;
+            }
+
+        }
+        System.out.println(Arrays.toString(inputArray5) + " - input array; чистая прибыль за месяцы");
+        System.out.println(Arrays.toString(outputArray5) + " - output array; количество месяцев рентабельности");
     }
 }
