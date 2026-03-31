7dc510f56da26802ba308c085b79fbe061aaffdd
Dens <dadadadenns1@gmail.com>
2026-03-31 15:02:53 +1000

    "homework 7" Изменен порядок вывода

--- src/Main.java
+++ src/Main.java
@@ -13,57 +13,37 @@ public class Main {
         short[] massiveShort = new short[]{111, 222, 333, 444, 555, 666, 777, 888, 999};
 
         //part 2
-        for (int i = 0; ; i++) {
-            if (i == massiveByte.length - 1) {
-                System.out.println(massiveByte[i]);
-                break;
-            } else {
-                System.out.print(massiveByte[i] + ", ");
-            }
+        for (int i = 0; i < massiveByte.length - 1; i++) {//i начинается с первого элемента, если длина больше 2-ух
+            System.out.print(massiveByte[i] + ", ");
         }
+        System.out.println(massiveByte[massiveByte.length - 1]); //Последний элемент массива
 
-        for (int i = 0; ; i++) {
-            if (i == massiveFloat.length - 1) {
-                System.out.println(massiveFloat[i]);
-                break;
-            } else {
-                System.out.print(massiveFloat[i] + ", ");
-            }
+        for (int i = 0; i < massiveFloat.length - 1; i++) {
+            System.out.print(massiveFloat[i] + ", ");
         }
+        System.out.println(massiveFloat[massiveFloat.length - 1]);
 
-        for (int i = 0; ; i++) {
-            if (i == massiveShort.length - 1) {
-                System.out.println(massiveShort[i]);
-                break;
-            } else {
-                System.out.print(massiveShort[i] + ", ");
-            }
+        for (int i = 0; i < massiveShort.length - 1; i++) {
+            System.out.print(massiveShort[i] + ", ");
         }
+        System.out.println(massiveShort[massiveShort.length - 1]);
+
         //part 3
-        for (int i = massiveByte.length - 1; ; i--) {
-            if (i == 0) {
-                System.out.println(massiveByte[i]);
-                break;
-            } else {
-                System.out.print(massiveByte[i] + ", ");
-            }
+        for (int i = massiveByte.length - 1; i > 0; i--) {//i начинается с последнего элемента, если длина больше 2-ух
+            System.out.print(massiveByte[i] + ", ");
         }
-        for (int i = massiveFloat.length - 1; ; i--) {
-            if (i == 0) {
-                System.out.println(massiveFloat[i]);
-                break;
-            } else {
-                System.out.print(massiveFloat[i] + ", ");
-            }
+        System.out.println(massiveByte[0]);//первый элемент массива
+
+        for (int i = massiveFloat.length - 1; i > 0; i--) {
+            System.out.print(massiveFloat[i] + ", ");
         }
-        for (int i = massiveShort.length - 1; ; i--) {
-            if (i == 0) {
-                System.out.println(massiveShort[i]);
-                break;
-            } else {
-                System.out.print(massiveShort[i] + ", ");
-            }
+        System.out.println(massiveFloat[0]);
+
+        for (int i = massiveShort.length - 1; i > 0; i--) {
+            System.out.print(massiveShort[i] + ", ");
         }
+        System.out.println(massiveShort[0]);
+
         //part 4 Только первый массив
         for (int i = 0; i < massiveByte.length; i++) {
             if (massiveByte[i] % 2 != 0) {
